6e520f6ba0b7ee3a66381ae22c198d6642f9d96b
natnael-OQP <makiya112545@gmail.com>
2022-03-31 20:05:58 +0300

    add tailwinf config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        // extend: {
+        //     fontFamily: {
+        //         lato: ['Lato', ...defaultTheme.fontFamily.sans],
+        //     },
+        //     colors: {
+        //         primary: '#191A2E',
+        //         secondary: '#43A7C8',
+        //         color3: '#D3D7DA',
+        //         color4: '#75AEC7',
+        //     },
+        // },
+    },
+    plugins: [],
+}
