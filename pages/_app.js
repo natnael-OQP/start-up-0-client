4d149ed7d97e62c4a58ee74c459b61bf9b3bdeca
natnael-OQP <makiya112545@gmail.com>
2022-03-31 20:11:47 +0300

    add progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import Router from 'next/router'
+import axios from 'axios'
+import ProgressBar from '@badrap/bar-of-progress'
+import Layout from '../components/Layout'
 import '../styles/globals.css'
 
+axios.defaults.baseURL = 'https://'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const progress = new ProgressBar({
+        size: 1,
+        color: '#29e',
+        className: 'bar-of-progress',
+        delay: 80,
+    })
+    // route Change Start
+    Router.events.on('routeChangeStart', () => progress.start())
+    // route Change end
+    Router.events.on('routeChangeComplete', () => progress.finish())
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
 
 export default MyApp
