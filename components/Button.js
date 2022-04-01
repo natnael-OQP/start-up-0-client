de9017b40cdef236a5191cbb51afd8c8cdb78cc1
natnael-OQP <makiya112545@gmail.com>
2022-04-01 14:59:04 +0300

    button component created

---
+++ components/Button.js
@@ -0,0 +1,9 @@
+const AppButton = ({ title }) => {
+    return (
+        <button className="px-4 py-1 text-sm font-medium text-white bg-black rounded-full">
+            {title}
+        </button>
+    )
+}
+
+export default AppButton
