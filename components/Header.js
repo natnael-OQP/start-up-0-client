21c2e5d389ba315a21e97f5e2abbacc3efe9342e
natnael-OQP <makiya112545@gmail.com>
2022-04-01 12:39:42 +0300

    style header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,28 @@
+import Image from 'next/image'
+
 const Header = () => {
-    return <div>Header</div>
+    return (
+        <div className="flex items-center justify-between w-full h-12 px-10 shadow-sm">
+            <div className="relative w-40 h-8 cursor-pointer">
+                <Image
+                    src="/logo.webp"
+                    objectFit="cover"
+                    layout="fill"
+                    alt="logo"
+                />
+            </div>
+            <div className="">
+                <div className="relative w-10 h-10 overflow-hidden rounded-full cursor-pointer">
+                    <Image
+                        src="/profile.jpeg"
+                        objectFit="cover"
+                        layout="fill"
+                        alt="logo"
+                    />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
