bd1375c5e41320f0ede47efa1498b05cef9f518f
natnael-OQP <makiya112545@gmail.com>
2022-04-01 14:59:25 +0300

    header component created

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,9 @@
 import Image from 'next/image'
+import { BellIcon, BookmarkIcon } from '@heroicons/react/Outline'
+import AppButton from './Button'
 
 const Header = () => {
+    const user = false
     return (
         <div className="flex items-center justify-between w-full h-12 px-10 shadow-sm">
             <div className="relative w-40 h-8 cursor-pointer">
@@ -11,15 +14,28 @@ const Header = () => {
                     alt="logo"
                 />
             </div>
-            <div className="">
-                <div className="relative w-10 h-10 overflow-hidden rounded-full cursor-pointer">
-                    <Image
-                        src="/profile.jpeg"
-                        objectFit="cover"
-                        layout="fill"
-                        alt="logo"
-                    />
-                </div>
+            <div className="fx">
+                {user ? (
+                    <>
+                        <div className="mr-5 fx">
+                            <BellIcon className="w-6 h-6 text-gray-400" />
+                            <BookmarkIcon className="w-6 h-6 text-gray-400" />
+                        </div>
+                        <div className="relative w-10 h-10 overflow-hidden rounded-full cursor-pointer">
+                            <Image
+                                src="/profile.jpeg"
+                                objectFit="cover"
+                                layout="fill"
+                                alt="logo"
+                            />
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <AppButton title="Login" />
+                        <AppButton title="register" />
+                    </>
+                )}
             </div>
         </div>
     )
