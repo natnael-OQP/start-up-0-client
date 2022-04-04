7822caee1b2c87e0000e722148a37c14c904bf5c
natnael-OQP <makiya112545@gmail.com>
2022-04-04 17:58:56 +0300

    SavedPost created

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,15 @@
+import Profiles from './Profiles'
+import RecentlySaved from './RecentlySaved'
 import RecommendedTopics from './RecommendedTopics'
 import Search from './Search'
 
 const Sidebar = () => {
     return (
-        <div className="flex-[.3]  min-h-screen border-l-[1px] px-6 py-10 ">
+        <div className="flex-[.27]  min-h-screen border-l-[1px] px-6 py-14 sticky top-0 ">
             <Search />
             <RecommendedTopics />
+            <Profiles />
+            <RecentlySaved />
         </div>
     )
 }
