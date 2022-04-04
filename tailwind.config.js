8f1536530ba0871559c36d42d423e40d12ebbf9a
natnael-OQP <makiya112545@gmail.com>
2022-04-04 12:16:04 +0300

    import defaultThem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: [
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        // extend: {
-        //     fontFamily: {
-        //         lato: ['Lato', ...defaultTheme.fontFamily.sans],
-        //     },
-        //     colors: {
-        //         primary: '#191A2E',
-        //         secondary: '#43A7C8',
-        //         color3: '#D3D7DA',
-        //         color4: '#75AEC7',
-        //     },
-        // },
+        extend: {
+            fontFamily: {
+                lato: ['Lato', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                dark: '#0f0f0f',
+                light: '#f2f2f3',
+                medium: '#D3D7DA',
+            },
+        },
     },
     plugins: [],
 }
