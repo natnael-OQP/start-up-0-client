9a5efd4f3392ee85245ad985ce41925a519074e0
natnael-OQP <makiya112545@gmail.com>
2022-04-04 12:17:13 +0300

    add max width

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Header from './Header'
 
 const Layout = ({ children }) => {
     return (
-        <div>
+        <div className="mx-auto max-w-[1350px] ">
             <Header />
             <main>{children}</main>
         </div>
