e474331f858d15e0e5b2504cf64ca74502ed45ac
natnael-OQP <makiya112545@gmail.com>
2022-04-04 17:58:29 +0300

    RecommendedTopics RecentlySaved

--- components/RecommendedTopics.js
+++ components/RecommendedTopics.js
@@ -4,9 +4,9 @@ import Topics from './Topics'
 const RecommendedTopics = () => {
     return (
         <div className="border-t-[1px] w-full p-1">
-            <h3 className="pb-4 text-base font-semibold pt-7 font-lato text-dark">
+            <h2 className="pb-4 text-base font-semibold pt-7 font-lato text-dark">
                 Recommended topics
-            </h3>
+            </h2>
             <div className="flex flex-wrap justify-start pb-4 pl-2 pr-3 gap-x-3 gap-y-4 ">
                 <Topics title="Ios Developer" />
                 <Topics title="Recommended" />
